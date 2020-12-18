fedb0e113505c733d177cb651c180601a770da6e
luguoqiang <>
2020-12-18 17:24:40 +0800

    代码精简

--- app/src/main/java/com/lugq/nodeadapterdemo/lesson/LessonProvider1.kt
+++ app/src/main/java/com/lugq/nodeadapterdemo/lesson/LessonProvider1.kt
@@ -1,6 +1,5 @@
 package com.lugq.nodeadapterdemo.lesson
 
-import android.util.Log
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.blankj.utilcode.util.ConvertUtils
@@ -14,21 +13,15 @@ import org.greenrobot.eventbus.EventBus
  * 横向列表父级Item视图
  */
 class LessonProvider1 : BaseNodeProvider() {
-    companion object {
-        const val TAG = "FirstProvider"
-    }
-
     override val itemViewType: Int
         get() = 1
 
-    /**
-     * 返回一级节点布局
-     */
     override val layoutId: Int
         get() = R.layout.item_lesson1
 
     override fun convert(helper: BaseViewHolder, item: BaseNode) {
         val firstNode = item as LessonFirstNode
+        // 关键代码：做分组头部Item显示和隐藏操作
         if (firstNode.isYincang) {
             val view = helper.getView<ConstraintLayout>(R.id.llroot)
             view.layoutParams.width = 0
@@ -38,31 +31,19 @@ class LessonProvider1 : BaseNodeProvider() {
         }
     }
 
-    /****/
     override fun onClick(helper: BaseViewHolder, view: View, data: BaseNode, position: Int) {
         super.onClick(helper, view, data, position)
-        Log.i(TAG, "点击了$position")
-        Log.i(TAG, "点击了位置：${helper.adapterPosition}")
-        // 这里使用payload进行增量刷新（避免整个item刷新导致的闪烁，不自然）
-
         /**
          * 请问FirstNodeProvider中如何展开其中一个node，并且折叠其他所有node？（类似单选框）遍历折叠后再展开position会越界
          * https://github.com/CymChad/BaseRecyclerViewAdapterHelper/issues/3333
          */
-        // 需求1：
-        //getAdapter()?.expandOrCollapse(position, false, true, 110)
-
-        // 需求2：点击后收起其它的
+        // 点击后收起其它的
         val da = data as LessonFirstNode
         if (da.isExpanded) {
-            // 每次
-
             getAdapter()?.collapse(position)
         } else {
             EventBus.getDefault().post(data)
-
             getAdapter()?.expandAndCollapseOther(position)
-
         }
     }
 
